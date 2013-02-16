d2b11e42cadba21ea3b951718762f4fe3e8dcf07
Nathan Eidelson <nathan.eidelson@gmail.com>
2013-02-15 18:53:58 -0800

    Finished!
    Added icons.

--- popup.js
+++ popup.js
@@ -1,45 +1,31 @@
-// Written by Nathan Eidelson, 2/14/13
+/* Written by Nathan Eidelson, 2/14/13
+ */
 
-
-//In the event that a user clicks a link, open a new tab using the linked address.
+//In the event that a user clicks a link, open a new tab with the linked address.
 function click(e) {
-    chrome.tabs.create({url: e.target.href});
+    chrome.tabs.create({url: e.target.alt});
 }
 
-var dashboardGenerator = {
-    addLink: function(tag, url) {
-        document.write("<br>");
-        var a = document.createElement('a');
-        a.addEventListener('click', click);
-        var linkText = document.createTextNode(tag);
-        a.appendChild(linkText);
-        a.title = tag;
-        a.href = url;
-        document.body.appendChild(a);
-        document.write("<br>");
+var dashboardGenerator =
+{
+    //This function generates a clickable icon, given the title of the image and the url to link to.
+    addLink: function(tag, url)
+    {
+        var img = document.createElement('img');
+        img.src = chrome.extension.getURL(tag + ".png");
+        img.alt = url;
+        img.addEventListener('click', click);
+        document.body.appendChild(img);
     }
-    
-    
-    
-/*
-    addButton: function() {
-        var newButton = document.createElement('gmail');
-      //  var linkText = document.createTextNode("Gmail");
-        newButton.appendChild(linkText);
-        a.title = "Gmail";
-        a.href = "https://mail.google.com";
-        document.body.appendChild(a);
-    } */
-    
 };
 
-
-// Run our generation script as soon as the document's DOM is ready.
-document.addEventListener("DOMContentLoaded", function () {
-    dashboardGenerator.addLink("Gmail", "https://mail.google.com");
-    dashboardGenerator.addLink("Google Docs", "https://drive.google.com");
-    dashboardGenerator.addLink("Google Calendar", "https://calendar.google.com");
+//Run the link generation script as soon as the document's DOM is ready.
+document.addEventListener("DOMContentLoaded", function ()
+{
+    dashboardGenerator.addLink("Gmail", "https://mail.google.com")
+    dashboardGenerator.addLink("Drive", "https://drive.google.com");
+    dashboardGenerator.addLink("Calendar", "https://calendar.google.com");
     dashboardGenerator.addLink("StartX", "http://startx.stanford.edu/");
-    dashboardGenerator.addLink("StartX Feedback", "https://docs.google.com/a/sse.stanford.edu/spreadsheet/viewform?formkey=dHpkTHFONGlfSnRJWk5LVkd4dXVVS1E6MQ#gid=0");
+    dashboardGenerator.addLink("Feedback","https://docs.google.com/a/sse.stanford.edu/spreadsheet/viewform formkey=dHpkTHFONGlfSnRJWk5LVkd4dXVVS1E6MQ#gid=0");
 
 });
